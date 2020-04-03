827f28d973d2865c7b634bed9be3c2d8c0b74197
Rigel Kent Carbonel <rigel20.kent@gmail.com>
2020-04-03 17:55:39 +0800

    added base test

---
+++ tests/BaseTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Luigel\Paymongo\Tests;
+
+use Luigel\Paymongo\PaymongoServiceProvider;
+use Orchestra\Testbench\TestCase;
+
+class BaseTest extends TestCase
+{
+    protected const TEST_VISA_CARD_WITHOUT_3D_SECURE = '4343434343434345';
+
+    /**
+     * Test example to remove warning
+     *
+     * @test
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [PaymongoServiceProvider::class];
+    }
+}
