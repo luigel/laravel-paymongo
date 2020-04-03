181667c6c2706147b429111daa80b3d8aa6d7768
Rigel Kent Carbonel <rigel20.kent@gmail.com>
2020-04-03 19:42:32 +0800

    Added payment method functionality

--- src/Paymongo.php
+++ src/Paymongo.php
@@ -3,6 +3,7 @@
 namespace Luigel\Paymongo;
 
 use Luigel\Paymongo\Models\Payment;
+use Luigel\Paymongo\Models\PaymentMethod;
 use Luigel\Paymongo\Models\Source;
 use Luigel\Paymongo\Models\Token;
 use Luigel\Paymongo\Models\Webhook;
@@ -22,6 +23,7 @@ class Paymongo
     protected const ENDPOINT_PAYMENTS = 'payments/';
     protected const ENPDPOINT_SOURCES = 'sources/';
     protected const ENDPOINT_WEBHOOKS = 'webhooks/';
+    protected const ENDPOINT_PAYMENT_METHOD = 'payment_methods/';
     protected const SOURCE_GCASH = 'gcash';
 
     public function token()
@@ -49,6 +51,13 @@ class Paymongo
     {
         $this->apiUrl = self::BASE_API . self::ENDPOINT_WEBHOOKS;
         $this->returnModel = Webhook::class;
+        return $this;
+    }
+
+    public function paymentMethod()
+    {
+        $this->apiUrl = self::BASE_API . self::ENDPOINT_PAYMENT_METHOD;
+        $this->returnModel = PaymentMethod::class;
         return $this;
     }
 }
