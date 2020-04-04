ea66db4558339e2e3cf345238c6176f6cdcb0942
Rigel Kent Carbonel <rigel20.kent@gmail.com>
2020-04-04 09:37:09 +0800

    Removed tokens and payments and added payment intent

--- src/Paymongo.php
+++ src/Paymongo.php
@@ -2,10 +2,9 @@
 
 namespace Luigel\Paymongo;
 
-use Luigel\Paymongo\Models\Payment;
+use Luigel\Paymongo\Models\PaymentIntent;
 use Luigel\Paymongo\Models\PaymentMethod;
 use Luigel\Paymongo\Models\Source;
-use Luigel\Paymongo\Models\Token;
 use Luigel\Paymongo\Models\Webhook;
 use Luigel\Paymongo\Traits\Request;
 
@@ -19,26 +18,12 @@ class Paymongo
     protected $returnModel = '';
 
     protected const BASE_API = 'https://api.paymongo.com/v1/';
-    protected const ENPOINT_TOKEN = 'tokens/';
-    protected const ENDPOINT_PAYMENTS = 'payments/';
     protected const ENPDPOINT_SOURCES = 'sources/';
     protected const ENDPOINT_WEBHOOKS = 'webhooks/';
     protected const ENDPOINT_PAYMENT_METHOD = 'payment_methods/';
+    protected const ENDPOINT_PAYMENT_INTENT = 'payment_intents/';
     protected const SOURCE_GCASH = 'gcash';
-
-    public function token()
-    {
-        $this->apiUrl = self::BASE_API . self::ENPOINT_TOKEN;
-        $this->returnModel = Token::class;
-        return $this;
-    }
-
-    public function payment()
-    {
-        $this->apiUrl = self::BASE_API . self::ENDPOINT_PAYMENTS;
-        $this->returnModel = Payment::class;
-        return $this;
-    }
+    protected const SOURCE_GRAB_PAY = 'grab_pay';
 
     public function source()
     {
@@ -58,6 +43,13 @@ class Paymongo
     {
         $this->apiUrl = self::BASE_API . self::ENDPOINT_PAYMENT_METHOD;
         $this->returnModel = PaymentMethod::class;
+        return $this;
+    }
+
+    public function paymentIntent()
+    {
+        $this->apiUrl = self::BASE_API . self::ENDPOINT_PAYMENT_INTENT;
+        $this->returnModel = PaymentIntent::class;
         return $this;
     }
 }
