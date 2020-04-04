35feaca10b7edaf19aa6211307cc2eb2dec74810
Rigel Kent Carbonel <rigel20.kent@gmail.com>
2020-04-04 09:38:28 +0800

    Added test case for grabpay source

--- tests/SourceTest.php
+++ tests/SourceTest.php
@@ -3,13 +3,12 @@
 namespace Luigel\Paymongo\Tests;
 
 use Luigel\Paymongo\Facades\Paymongo;
-use Luigel\Paymongo\PaymongoServiceProvider;
 use Luigel\Paymongo\Models\Source;
 
-class SourceTest extends BaseTest
+class SourceTest extends BaseTestCase
 {
     /** @test */
-    public function it_can_create_a_source()
+    public function it_can_create_a_gcash_source()
     {
         $source = Paymongo::source()->create([
             'type' => 'gcash',
@@ -26,6 +25,34 @@ class SourceTest extends BaseTest
         $this->assertEquals('source', $source->type);
 
         $this->assertEquals('gcash', $source->source_type);
+
+        $this->assertEquals('100.00', $source->amount);
+
+        $this->assertEquals([
+            'success' => 'http://localhost/success',
+            'failed' => 'http://localhost/failed',
+            'checkout_url' => $source->redirect['checkout_url']
+        ], $source->redirect);
+    }
+
+    /** @test */
+    public function it_can_create_a_grab_pay_source()
+    {
+        $source = Paymongo::source()->create([
+            'type' => 'grab_pay',
+            'amount' => 100.00,
+            'currency' => 'PHP',
+            'redirect' => [
+                'success' => 'http://localhost/success',
+                'failed' => 'http://localhost/failed'
+            ]
+        ]);
+
+        $this->assertInstanceOf(Source::class, $source);
+
+        $this->assertEquals('source', $source->type);
+
+        $this->assertEquals('grab_pay', $source->source_type);
 
         $this->assertEquals('100.00', $source->amount);
 
