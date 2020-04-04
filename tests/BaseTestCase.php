fb217093c6a9fdf4de9685eb435e1f7828d6c1bf
Rigel Kent Carbonel <rigel20.kent@gmail.com>
2020-04-04 09:35:36 +0800

    renamed BaseTest to BaseTestCase

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -5,20 +5,10 @@ namespace Luigel\Paymongo\Tests;
 use Luigel\Paymongo\PaymongoServiceProvider;
 use Orchestra\Testbench\TestCase;
 
-class BaseTest extends TestCase
+class BaseTestCase extends TestCase
 {
     protected const TEST_VISA_CARD_WITHOUT_3D_SECURE = '4343434343434345';
     protected const TEST_VISA_CARD_WITHOUT_3D_SECURE_LAST_4 = '4345';
-
-    /**
-     * Test example to remove warning
-     *
-     * @test
-     */
-    public function testExample()
-    {
-        $this->assertTrue(true);
-    }
 
     protected function getPackageProviders($app)
     {
