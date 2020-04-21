23ba908e488cd997617adb731aa7c4785f7ba43d
Rigel Kent Carbonel <rigel20.kent@gmail.com>
2020-04-21 11:07:26 +0800

    Added Token Test

---
+++ tests/TokenTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Luigel\Paymongo\Tests;
+
+use Luigel\Paymongo\Exceptions\BadRequestException;
+use Luigel\Paymongo\Facades\Paymongo;
+use Orchestra\Testbench\TestCase;
+use Luigel\Paymongo\PaymongoServiceProvider;
+use Luigel\Paymongo\Models\Card;
+use Luigel\Paymongo\Models\Token;
+
+class TokenTest extends BaseTestCase
+{
+    /** @test */
+    public function it_can_create_token()
+    {
+        $token = Paymongo::token()
+            ->create([
+                'number' => $this::TEST_VISA_CARD_WITHOUT_3D_SECURE,
+                'exp_month' => 12,
+                'exp_year' => 25,
+                'cvc' => "123",
+                'billing' => [
+                    'address' => [
+                        'line1' => 'Test Address',
+                        'city' => 'Cebu City',
+                        'postal_code' => '6000',
+                        'country' => 'PH'
+                    ],
+                    'name' => 'Rigel Kent Carbonel',
+                    'email' => 'rigel20.kent@gmail.com',
+                    'phone' => '928392893'
+                ]
+            ]);
+
+        $this->assertInstanceOf(Token::class, $token);
+        $this->assertEquals($token->getCard()['last4'], '4345');
+        $this->assertEquals($token->getCard()['exp_month'], 12);
+        $this->assertEquals($token->getBillingName(), 'Rigel Kent Carbonel');
+    }
+
+    /** @test */
+    public function it_cannot_create_token_with_invalid_data()
+    {
+        $this->expectException(BadRequestException::class);
+
+        Paymongo::token()
+            ->create([
+                'number' => '424242424242424222',
+                'exp_month' => 12,
+                'exp_year' => 25,
+                'cvc' => "123",
+            ]);
+    }
+
+    /** @test */
+    public function it_can_retrieve_token()
+    {
+        $createdToken = Paymongo::token()
+            ->create([
+                'number' => $this::TEST_VISA_CARD_WITHOUT_3D_SECURE,
+                'exp_month' => 12,
+                'exp_year' => 25,
+                'cvc' => "123",
+                'billing' => [
+                    'address' => [
+                        'line1' => 'Test Address',
+                        'city' => 'Cebu City',
+                        'postal_code' => '6000',
+                        'country' => 'PH'
+                    ],
+                    'name' => 'Rigel Kent Carbonel',
+                    'email' => 'rigel20.kent@gmail.com',
+                    'phone' => '928392893'
+                ]
+            ]);
+
+        $token = Paymongo::token()->find($createdToken->getId());
+
+        $this->assertEquals($createdToken, $token);
+    }
+}
