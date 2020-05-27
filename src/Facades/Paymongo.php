f65b8fb300116f1d68cd55fee9fab014c43a8ac3
Rigel Kent Carbonel <rigel20.kent@gmail.com>
2020-05-27 20:56:04 +0800

    docs: added docs in facade

--- src/Facades/Paymongo.php
+++ src/Facades/Paymongo.php
@@ -5,7 +5,18 @@ namespace Luigel\Paymongo\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @see \Luigel\Paymongo\Skeleton\SkeletonClass
+ * @method static \Luigel\Paymongo\Paymongo payment()
+ * @method static \Luigel\Paymongo\Paymongo paymentIntent()
+ * @method static \Luigel\Paymongo\Paymongo source()
+ * @method static \Luigel\Paymongo\Paymongo webhook()
+ * @method static \Luigel\Paymongo\Paymongo paymentMethod()
+ * @method static \Luigel\Paymongo\Paymongo token() @deprecated 1.2.0
+ * @method static mixed create(array $payload)
+ * @method static mixed find(string $id)
+ * @method static mixed all()
+ * @method static mixed enable()
+ * @method static mixed disable()
+ * 
  */
 class Paymongo extends Facade
 {
