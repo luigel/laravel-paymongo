ab8a141f6b742a34ddf651dcc98930476f7117b8
Rigel Kent Carbonel <luigel@users.noreply.github.com>
2020-11-01 06:32:46 +0000

    Apply fixes from StyleCI

--- tests/BaseModelTest.php
+++ tests/BaseModelTest.php
@@ -11,48 +11,48 @@ class BaseModelTest extends BaseTestCase
     {
         $model = new BaseModel();
         $data = [
-            "id" => "pay_4exEiTS1V6f2V28zo33S5v4R",
-            "type" => "payment",
-            "attributes" => [
-              "access_url" => null,
-              "amount" => 10000,
-              "balance_transaction_id" => "bal_txn_2BVC9Xe4Hu9egYfoEvRcQYXS",
-              "billing" => [
-                "address" => [
-                  "city" => "Cebu City",
-                  "country" => "PH",
-                  "line1" => "Test Address",
-                  "line2" => null,
-                  "postal_code" => "6000",
-                  "state" => null,
-                ],
-                "email" => "rigel20.kent@gmail.com",
-                "name" => "Rigel Kent Carbonel",
-                "phone" => "928392893",
+            'id' => 'pay_4exEiTS1V6f2V28zo33S5v4R',
+            'type' => 'payment',
+            'attributes' => [
+                'access_url' => null,
+                'amount' => 10000,
+                'balance_transaction_id' => 'bal_txn_2BVC9Xe4Hu9egYfoEvRcQYXS',
+                'billing' => [
+                    'address' => [
+                        'city' => 'Cebu City',
+                        'country' => 'PH',
+                        'line1' => 'Test Address',
+                        'line2' => null,
+                        'postal_code' => '6000',
+                        'state' => null,
+                    ],
+                    'email' => 'rigel20.kent@gmail.com',
+                    'name' => 'Rigel Kent Carbonel',
+                    'phone' => '928392893',
               ],
-              "currency" => "PHP",
-              "description" => "Testing payment",
-              "disputed" => false,
-              "external_reference_number" => null,
-              "fee" => 1850,
-              "foreign_fee" => 100,
-              "livemode" => false,
-              "net_amount" => 8050,
-              "payout" => null,
-              "source" => [
-                "id" => "tok_ayYr6JnADwLnmA4bGvmg4Hzx",
-                "type" => "token",
-                "brand" => "visa",
-                "country" => "US",
-                "last4" => "4345",
+              'currency' => 'PHP',
+              'description' => 'Testing payment',
+              'disputed' => false,
+              'external_reference_number' => null,
+              'fee' => 1850,
+              'foreign_fee' => 100,
+              'livemode' => false,
+              'net_amount' => 8050,
+              'payout' => null,
+              'source' => [
+                  'id' => 'tok_ayYr6JnADwLnmA4bGvmg4Hzx',
+                  'type' => 'token',
+                  'brand' => 'visa',
+                  'country' => 'US',
+                  'last4' => '4345',
               ],
-              "statement_descriptor" => "Test Paymongo",
-              "status" => "paid",
-              "refunds" => [],
-              "available_at" => 1604653200,
-              "created_at" => 1604210517,
-              "paid_at" => 1604210517,
-              "updated_at" => 1604210517,
+              'statement_descriptor' => 'Test Paymongo',
+              'status' => 'paid',
+              'refunds' => [],
+              'available_at' => 1604653200,
+              'created_at' => 1604210517,
+              'paid_at' => 1604210517,
+              'updated_at' => 1604210517,
             ],
         ];
         $baseModel = $model->setData($data);
@@ -65,12 +65,12 @@ class BaseModelTest extends BaseTestCase
         $this->assertEquals(100.00, $baseModel->getAmount());
         $this->assertEquals(100.00, $baseModel->amount);
         $this->assertEquals([
-            "city" => "Cebu City",
-            "country" => "PH",
-            "line1" => "Test Address",
-            "line2" => null,
-            "postal_code" => "6000",
-            "state" => null,
+            'city' => 'Cebu City',
+            'country' => 'PH',
+            'line1' => 'Test Address',
+            'line2' => null,
+            'postal_code' => '6000',
+            'state' => null,
         ], $baseModel->getBillingAddress());
     }
 }
